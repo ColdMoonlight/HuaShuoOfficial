ae4887d419e44e63d8e354de50e7d82327e7f9f5
lenovo <mingyueqingl@163.com>
2021-08-12 08:39:25 +0800

    1245

--- src/main/java/com/atguigu/controller/CrmCatalogController.java
+++ src/main/java/com/atguigu/controller/CrmCatalogController.java
@@ -14,14 +14,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.atguigu.bean.CrmAdmin;
 import com.atguigu.bean.CrmCatalog;
-import com.atguigu.common.Const;
 import com.atguigu.common.Msg;
-import com.atguigu.common.TokenCache;
 import com.atguigu.service.CrmAdminService;
 import com.atguigu.service.CrmCatalogService;
 import com.atguigu.utils.DateUtil;
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
 
 @Controller
 @RequestMapping("/CrmCatalog")
@@ -99,6 +95,84 @@ public class CrmCatalogController {
 		}
 			
 		return Msg.success().add("CrmCatalogdownFirst", CrmCatalogdownFirst).add("CrmCatalogSuperList", CrmCatalogSuperList);
+	}
+	
+	/**
+	 * 2.0
+	 * @author 20210810
+	 * @param CrmCatalog
+	 * @exception 创建新用户
+	 * */
+	@RequestMapping(value="/save",method=RequestMethod.POST)
+	@ResponseBody
+	public Msg insertSelective(HttpServletResponse rep,HttpServletRequest res,HttpSession session,
+			@RequestBody CrmCatalog crmCatalogReg){
+		//接收参数信息 
+		
+		Integer cId = crmCatalogReg.getCatalogId();
+		String nowTime = DateUtil.strTime14s();
+		crmCatalogReg.setCatalogMotifytime(nowTime);
+		if(cId==null){
+			crmCatalogReg.setCatalogCreatetime(nowTime);
+			crmCatalogService.insertSelective(crmCatalogReg);
+			
+		}else{
+			crmCatalogService.updateByPrimaryKeySelective(crmCatalogReg);
+		}
+		
+		return Msg.success().add("resMsg", "创建成功");
+		
+	}
+	
+	/**
+	 * 3.0
+	 * @author 20210810
+	 * @param CrmAdmin
+	 * @exception 查看单个用户
+	 * */
+	@RequestMapping(value="/GetOneCrmCatalogDetail",method=RequestMethod.POST)
+	@ResponseBody
+	public Msg GetOneCrmCatalogDetail(HttpServletResponse rep,HttpServletRequest res,HttpSession session,
+			@RequestBody CrmCatalog crmCatalogReg){
+		
+		//通过id 查询单个用户详情
+		CrmCatalog crmCatalogRes = crmCatalogService.selectByPrimaryKey(crmCatalogReg.getCatalogId());
+		return Msg.success().add("crmCatalog", crmCatalogRes);
+		
+	}
+	
+	
+	/**
+	 * 4.0
+	 * @author 20210810
+	 * @param CrmAdmin
+	 * @exception 查看单个用户
+	 * */
+	@RequestMapping(value="/delete",method=RequestMethod.POST)
+	@ResponseBody
+	public Msg delete(HttpServletResponse rep,HttpServletRequest res,HttpSession session,
+			@RequestBody CrmCatalog crmCatalogReg){
+		
+		//通过id 查询单个用户详情
+		crmCatalogService.deleteByPrimaryKey(crmCatalogReg.getCatalogId());
+		return Msg.success().add("resMsg", "创建成功");
+		
+	}
+	
+	/**
+	 * 4.0
+	 * @author 20210810
+	 * @param CrmAdmin
+	 * @exception 获取下拉菜单
+	 * */
+	@RequestMapping(value="/getDownList")
+	@ResponseBody
+	public Msg getDownList(HttpServletResponse rep,HttpServletRequest res,HttpSession session){
+		
+		//通过id 查询单个用户详情
+		crmCatalogService.deleteByPrimaryKey(crmCatalogReg.getCatalogId());
+		return Msg.success().add("resMsg", "创建成功");
+		
 	}
 	
 }
