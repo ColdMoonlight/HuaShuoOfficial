3cdd7b6b6251a00ab074069c35a8f7f0114e8ee6
Swgd-ysl <Shinelon@DESKTOP-L8ICF2P>
2021-08-17 16:18:51 +0800

    tb_crm_shoproom添加字段

--- src/main/java/com/atguigu/bean/CrmShopRoom.java
+++ src/main/java/com/atguigu/bean/CrmShopRoom.java
@@ -4,6 +4,10 @@ public class CrmShopRoom {
     private Integer shoproomId;
 
     private String shoproomName;
+    
+    private Integer shoproomIdDepartmentId;
+
+    private String shoproomNameDepartmentName;
 
     private String shoproomCreatetime;
 
@@ -25,7 +29,23 @@ public class CrmShopRoom {
         this.shoproomName = shoproomName == null ? null : shoproomName.trim();
     }
 
-    public String getShoproomCreatetime() {
+    public Integer getShoproomIdDepartmentId() {
+		return shoproomIdDepartmentId;
+	}
+
+	public void setShoproomIdDepartmentId(Integer shoproomIdDepartmentId) {
+		this.shoproomIdDepartmentId = shoproomIdDepartmentId;
+	}
+
+	public String getShoproomNameDepartmentName() {
+		return shoproomNameDepartmentName;
+	}
+
+	public void setShoproomNameDepartmentName(String shoproomNameDepartmentName) {
+		this.shoproomNameDepartmentName = shoproomNameDepartmentName;
+	}
+
+	public String getShoproomCreatetime() {
         return shoproomCreatetime;
     }
 
@@ -40,4 +60,26 @@ public class CrmShopRoom {
     public void setShoproomMotifytime(String shoproomMotifytime) {
         this.shoproomMotifytime = shoproomMotifytime == null ? null : shoproomMotifytime.trim();
     }
+
+	public CrmShopRoom() {
+		super();
+	}
+
+	public CrmShopRoom(Integer shoproomId, String shoproomName, Integer shoproomIdDepartmentId,
+			String shoproomNameDepartmentName, String shoproomCreatetime, String shoproomMotifytime) {
+		super();
+		this.shoproomId = shoproomId;
+		this.shoproomName = shoproomName;
+		this.shoproomIdDepartmentId = shoproomIdDepartmentId;
+		this.shoproomNameDepartmentName = shoproomNameDepartmentName;
+		this.shoproomCreatetime = shoproomCreatetime;
+		this.shoproomMotifytime = shoproomMotifytime;
+	}
+
+	@Override
+	public String toString() {
+		return "CrmShopRoom [shoproomId=" + shoproomId + ", shoproomName=" + shoproomName + ", shoproomIdDepartmentId="
+				+ shoproomIdDepartmentId + ", shoproomNameDepartmentName=" + shoproomNameDepartmentName
+				+ ", shoproomCreatetime=" + shoproomCreatetime + ", shoproomMotifytime=" + shoproomMotifytime + "]";
+	}
 }
